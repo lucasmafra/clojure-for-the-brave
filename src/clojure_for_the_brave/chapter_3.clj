8fc54b39495870c298269e2fee694bfacb3a4f5b
Lucas Mafra <lucas.mafra95@gmail.com>
2018-10-21 10:40:59 -0200

    Exercise 2

--- src/clojure_for_the_brave/chapter_3.clj
+++ src/clojure_for_the_brave/chapter_3.clj
@@ -1,9 +1,15 @@
 (ns clojure-for-the-brave.chapter-3)
 
-(defn ex_1
+; 1) Use the str, vector, list, hash-map, and hash-set functions.
+(defn exercise_1
   []
   (println (str "Hello, " "clojure"))
   (println (vector 1 2 3))
   (println (list 1 2 3))
   (println (hash-map :a 1 :b 2))
   (println (hash-set 1 2 2 3 4)))
+
+; 2) Write a function that takes a number and adds 100 to it.
+(defn add100
+  [n]
+  (+ n 100))
