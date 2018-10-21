a18a1c1a9e47745700c11e1351403b75dbb68e73
Lucas Mafra <lucas.mafra95@gmail.com>
2018-10-21 11:01:00 -0200

    Exercise 4

--- src/clojure_for_the_brave/chapter_3.clj
+++ src/clojure_for_the_brave/chapter_3.clj
@@ -14,7 +14,14 @@
   [n]
   (+ n 100))
 
-; 3. Write a function, dec-maker, that works exactly like the function inc-maker except with subtraction:
+; 3. Write a function, dec-maker, that works exactly like the function inc-maker
+; except with subtraction:
 (defn dec-maker
   [size]
   #(- % size))
+
+; 4. Write a function, mapset, that works like map except the return value is a
+; set:
+(defn mapset
+  [func arr]
+  (set (map func arr)))
