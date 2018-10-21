bd18c5231d15f86a2318680bc8a094b772fe1058
Lucas Mafra <lucas.mafra95@gmail.com>
2018-10-20 22:27:00 -0300

    Exercise 1

---
+++ src/clojure_for_the_brave/chapter_3.clj
@@ -0,0 +1,9 @@
+(ns clojure-for-the-brave.chapter-3)
+
+(defn ex_1
+  []
+  (println (str "Hello, " "clojure"))
+  (println (vector 1 2 3))
+  (println (list 1 2 3))
+  (println (hash-map :a 1 :b 2))
+  (println (hash-set 1 2 2 3 4)))
